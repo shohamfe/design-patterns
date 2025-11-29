7dc407d51f4e59845cda5445438a65840b12377e
AlmogGabay <almognv@gmail.com>
2025-11-29 12:11:52 +0200

    add postDeatails class in models folder

---
+++ A26 Ex01 ShohamFellner 207500653 AlmogNagauker 319123527/FacebookWinFormsApp/Logic/Models/PostDeatails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures.Logic.Models
+{
+    public class PostDeatails
+    {
+        public string FullName { get; set; }
+
+        public DateTime PostTime { get; set; }
+
+        public string PostText { get; set; }
+
+        public PostDeatails(string i_FullName, DateTime i_PostTime, string i_PostText)
+        {
+            FullName = i_FullName;
+            PostTime = i_PostTime;
+            PostText = i_PostText; 
+        }
+    }
+}
