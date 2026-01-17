ef7d4e664d62211927d32623f1d5cb6ad1974bf8
AlmogGabay <almognv@gmail.com>
2026-01-17 19:20:23 +0200

    Refactor FeedPageComponent to simplify post loading
    Removed incremental batch loading and scrolling logic from FeedPageComponent. Now, all posts are loaded at once using PostsGridComponent, streamlining the UI update process and reducing complexity.

--- A26 Ex02 ShohamFellner 207500653 AlmogNagauker 319123527/FacebookWinFormsApp/UI/Components/FeedPageComponent.cs
+++ A26 Ex02 ShohamFellner 207500653 AlmogNagauker 319123527/FacebookWinFormsApp/UI/Components/FeedPageComponent.cs
@@ -10,32 +10,10 @@ namespace BasicFacebookFeatures.UI.Components
 {
     public partial class FeedPageComponent : UserControl
     {
-        private bool m_IsLoading = false;
-        private List<PostDetails> m_AllPosts;
-        private int m_currentIndex = 0;
-        private const int k_batchSize = 10;
         private PostsGridComponent m_PostsGridComponent;
-
         public FeedPageComponent()
         {
             InitializeComponent();
-            this.Main.Scroll += (s, e) => checkAndLoadMore();
-            this.Main.MouseWheel += (s, e) => checkAndLoadMore();
-        }
-
-        private void checkAndLoadMore()
-        {
-            if (m_IsLoading)
-            {
-                return;
-            }
-            int scrollPosition = Main.VerticalScroll.Value + Main.Height;
-            int totalHeight = postsPanel.Height;
-
-            if (scrollPosition >= totalHeight - 100)
-            {
-                new Thread(loadNextBatch).Start();
-            }
         }
 
         private void FeedPageComponent_Load(object sender, EventArgs e)
@@ -45,78 +23,36 @@ namespace BasicFacebookFeatures.UI.Components
 
         private void fetchFeedData()
         {
-            m_AllPosts = FacebookSession.Instance.FeedPosts;
-            m_currentIndex = 0;
-            loadNextBatch();
+            List<PostDetails> posts = FacebookSession.Instance.FeedPosts;
+            PostGridDetails postsGridData = new PostGridDetails("Feed", posts);
+            this.Invoke(new Action(() => FeedPage_Load(postsGridData)));
         }
 
-        private void loadNextBatch()
+        private void FeedPage_Load(PostGridDetails i_PostsGridData)
         {
-            if (m_AllPosts == null || m_currentIndex >= m_AllPosts.Count)
+            this.SuspendLayout();
+            postsPanel.SuspendLayout();
+
+            try
             {
-                return;
-            }
+                if (m_PostsGridComponent == null || m_PostsGridComponent.IsDisposed)
+                {
+                    m_PostsGridComponent = new PostsGridComponent();
+                }
 
-            m_IsLoading = true;
-            int end = Math.Min(m_currentIndex + k_batchSize, m_AllPosts.Count);
+                m_PostsGridComponent.Populate(i_PostsGridData);
+                ThemeColorizer.ApplyTheme(m_PostsGridComponent, ThemeManager.Instance.CurrentTheme);
 
-            for (int i = m_currentIndex; i < end; i++)
-            {
-                PostDetails currentPost = m_AllPosts[i];
-                this.Invoke(new Action(() => addSinglePostToUI(currentPost)));
-                m_currentIndex++;
-                Thread.Sleep(5);
-            }
-
-            m_IsLoading = false;
-        }
-
-        private void addSinglePostToUI(PostDetails i_Post)
-        {
-            if (m_currentIndex == 0)
-            {
+                m_PostsGridComponent.Dock = DockStyle.Fill;
                 postsPanel.Controls.Clear();
+                postsPanel.Controls.Add(m_PostsGridComponent);
             }
 
-            List<PostDetails> singlePostList = new List<PostDetails> { i_Post };
-            PostGridDetails singleData = new PostGridDetails("Feed", singlePostList);
-
-            PostsGridComponent postGrid = new PostsGridComponent();
-            postGrid.Populate(singleData);
-            ThemeColorizer.ApplyTheme(postGrid, ThemeManager.Instance.CurrentTheme);
-
-            postsPanel.AutoSize = true;
-            postsPanel.Controls.Add(postGrid);
+            finally
+            {
+                postsPanel.ResumeLayout();
+                this.ResumeLayout();
+            }
         }
-
-
-
-        //private void FeedPage_Load(PostGridDetails i_PostsGridData)
-        //{
-        //    this.SuspendLayout();
-        //    postsPanel.SuspendLayout();
-
-        //    try
-        //    {
-        //        if (m_PostsGridComponent == null || m_PostsGridComponent.IsDisposed)
-        //        {
-        //            m_PostsGridComponent = new PostsGridComponent();
-        //        }
-
-        //        m_PostsGridComponent.Populate(i_PostsGridData);
-        //        ThemeColorizer.ApplyTheme(m_PostsGridComponent, ThemeManager.Instance.CurrentTheme);
-
-        //        m_PostsGridComponent.Dock = DockStyle.Fill;
-        //        postsPanel.Controls.Clear();
-        //        postsPanel.Controls.Add(m_PostsGridComponent);
-        //    }
-
-        //    finally
-        //    {
-        //        postsPanel.ResumeLayout();
-        //        this.ResumeLayout();
-        //    }
-        //}
     }
 }
-
