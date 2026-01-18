04b29eabf1a5885a52abe02daa6c645fead3bbc1
shoham98 <shoham.fe@gmail.com>
2026-01-18 20:10:23 +0200

    Refactor login logic to FacebookSession
    Moved the login process from LoginComponent to FacebookSession, encapsulating authentication and error handling. LoginComponent now calls FacebookSession.Login, improving separation of concerns and code maintainability.

--- A26 Ex02 ShohamFellner 207500653 AlmogNagauker 319123527/FacebookWinFormsApp/UI/Components/LoginComponent.cs
+++ A26 Ex02 ShohamFellner 207500653 AlmogNagauker 319123527/FacebookWinFormsApp/UI/Components/LoginComponent.cs
@@ -28,46 +28,24 @@ namespace BasicFacebookFeatures.UI.Components
 
         private void login()
         {
+            string errorMessage = string.Empty;
+
             LabelError.Visible = false;
 
             try
             {
-                FacebookSession.Instance.LoginResult = FacebookService.Login(
-                    textBoxAppID.Text,
-                    /// requested permissions:
-                    "email",
-                    "user_birthday",
-                    "user_gender",
-                    "public_profile",
-                    "user_friends",
-                    "user_photos",
-                    "user_posts",
-                    "user_videos",
-                    "user_likes",
-                    "user_link"
-                    /// add any relevant permissions
-                    );
+                bool isLoginSuccess = FacebookSession.Instance.Login(textBoxAppID.Text, out errorMessage);
 
-
-                if (FacebookSession.Instance.User != null && string.IsNullOrEmpty(FacebookSession.Instance.LoginResult.ErrorMessage))
+                if (isLoginSuccess)
                 {
                     afterLogin();
                 }
                 else
                 {
                     this.ActiveControl = null;
-                    FacebookSession.Instance.LoginResult = null;
 
+                    throw new Exception(errorMessage);
                 }
-
-                // TODO: replace connect with comment
-                //Almog's token for testing
-                //FacebookSession.Instance.LoginResult = FacebookService.Connect("EAAQykGq3ihQBQGP2CFT3Ls7PDH3kr5IwdqFTCUHPtJqNVzNSUf3QPHCfP645D8SbdIs0e67HZBaDphXNmMpfcWpDZAncLC7mmZCxa6lyFXRNPZBhjfypQHI617zad400CeTTngBjTTJZBoXU6dW6lgZCgSf5nJSQaRyjFkSp5hDqmyuipfjb0BbRogAUgI0vNSRuMv2XPDDsjTmyx3HfIC75Di8onf5w8RqDXz2VdCdXoBRRYemOZCYoBgKc3Dc");
-
-                //Shoham's token for testing
-                //FacebookSession.Instance.LoginResult = FacebookService.Connect("EAAQykGq3ihQBQKLioVB0PCZBpsTzyZB15gfZC0ZBZBqPLZCptFjto3knsBvQ2jqwMFq3dazMOD3KCg5gJO45xSsIPstea2UzGi8YzM7i63dwNH4FB51mw7dHAP1aZChjSQ2QGcGk0VqJcCFZA8QarjoTXGm2zyNvR4InOha6mLikTUCXbfbRA9nVDLZAZBShjJMnzCYVqg8l8NMmK9aflDEdeBPsFslOYhS5zdg6cPHmvNPHWG0lQw8WCEj5EQZArZCbRVu8x8Et46IdCOIUuwZDZD");
-
-                //afterLogin();
             }
             catch (Exception ex)
             {
